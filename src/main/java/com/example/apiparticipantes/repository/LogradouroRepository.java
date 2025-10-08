4b202b5a6e1c9d69f60e1b4c4011e00e92bb68e4
Jamile <jamilehassensa@gmail.com>
2025-10-08 20:06:47 -0300

    Finalizando

--- src/main/java/com/example/apiparticipantes/repository/LogradouroRepository.java
+++ src/main/java/com/example/apiparticipantes/repository/LogradouroRepository.java
@@ -3,5 +3,8 @@ package com.example.apiparticipantes.repository;
 import com.example.apiparticipantes.model.Logradouro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LogradouroRepository extends JpaRepository<Logradouro, String> {
+    Optional<Logradouro> findByNomeLogradouro(String nomeLogradouro);
 }

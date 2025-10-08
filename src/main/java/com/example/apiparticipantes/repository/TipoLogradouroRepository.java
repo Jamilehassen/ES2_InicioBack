e2edc7680cf4a5546caea4a64e45c694abf835f6
Jamile <jamilehassensa@gmail.com>
2025-10-08 19:42:50 -0300

    first commit

---
+++ src/main/java/com/example/apiparticipantes/repository/TipoLogradouroRepository.java
@@ -0,0 +1,7 @@
+package com.example.apiparticipantes.repository;
+
+import com.example.apiparticipantes.model.TipoLogradouro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoLogradouroRepository extends JpaRepository<TipoLogradouro, String> {
+}
